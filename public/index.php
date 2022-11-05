2bb877ff060dc248850ea5d637edbb7d6791d9bd
Yura Birukov <birukov.yura@gmail.com>
2022-11-05 14:14:43 +0300

    edit index.php

--- public/index.php
+++ public/index.php
@@ -8,10 +8,12 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
 });
+
+$app->post('/users', function ($request, $response) {
+    return $response->write('POST /users');
+});
+
 $app->run();

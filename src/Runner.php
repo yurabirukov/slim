f740829d1c10796c0d56f8baafd85a95dd819fa5
Yura Birukov <birukov.yura@gmail.com>
2022-11-11 14:19:48 +0300

    Runner.php

---
+++ src/Runner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hexlet\Php\Runner;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+        return strtoupper($name);
+    });
+
+    return $collection;
+}
+?>
